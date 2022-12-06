95082a390417bcb9342ea563e84222f3b6db850d
pcm17 <petermccloskey1@gmail.com>
2022-12-05 20:00:45 -0500

    remove food lists

--- plantvillage-cheese-s3/src/main/java/dependency/plantvillage/com/cheeses3/SelectPortionSizeActivity.java
+++ plantvillage-cheese-s3/src/main/java/dependency/plantvillage/com/cheeses3/SelectPortionSizeActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class SelectPlatePortionSizeActivity extends AppCompatActivity {
+public class SelectPortionSizeActivity extends AppCompatActivity {
     Button btn_small;
     Button btn_medium;
     Button btn_large;
@@ -17,9 +17,8 @@ public class SelectPlatePortionSizeActivity extends AppCompatActivity {
 
     String food_name;
     String portion_size;
-    ArrayList<String> food_list;
+
     ArrayList<String> portion_list;
-    ArrayList<Integer> food_id_list;
     ArrayList<Integer> portion_id_list;
     int portion_id;
     int picType;
@@ -27,7 +26,8 @@ public class SelectPlatePortionSizeActivity extends AppCompatActivity {
     int numFoodsInRecipe;
     String dishType;
     ArrayList<String> recipeCodeList;
-    ArrayList<String> recipeList;
+
+    ArrayList<String> recipeNameList;
 
     String filename = "";
 
@@ -40,8 +40,6 @@ public class SelectPlatePortionSizeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_plate_portion_size);
         Intent intent=getIntent();
         food_name= intent.getStringExtra("food_name");
-        food_list = intent.getStringArrayListExtra("food_list");
-        food_id_list = intent.getIntegerArrayListExtra("food_id_list");
         portion_id_list = intent.getIntegerArrayListExtra("portion_id_list");
         picType = intent.getIntExtra("picType", 0);
         portion_list = intent.getStringArrayListExtra("portion_list");
@@ -50,7 +48,7 @@ public class SelectPlatePortionSizeActivity extends AppCompatActivity {
         dishType = intent.getStringExtra("dishType");
         recipeCodeList = intent.getStringArrayListExtra("recipeCodeList");
         filename = intent.getStringExtra("filename");
-        recipeList = intent.getStringArrayListExtra("recipeList");
+        recipeNameList = intent.getStringArrayListExtra("recipeNameList");
 
         btn_small = (Button) findViewById(R.id.btn_small);
         btn_medium = (Button) findViewById(R.id.btn_medium);
@@ -110,16 +108,14 @@ public class SelectPlatePortionSizeActivity extends AppCompatActivity {
 
     public void selectAddFoodCapture(View view) {
         Intent intent;
-        if(dishType.equals("IM") || dishType.equals("FM")) {
-            intent = new Intent(this, CaptureImageUploadImFmActivity.class);
-        } else {
-            intent = new Intent(this, CaptureImageUploadActivity.class);
-        }        for (int i = 0;i<numFoodsInRecipe;i++) {
+
+        intent = new Intent(this, CaptureImageUploadImFmActivity.class);
+        for (int i = 0;i<numFoodsInRecipe;i++) {
             portion_list.add(portion_size);
             portion_id_list.add(portion_id);
         }
-        intent.putExtra("food_list", food_list);
-        intent.putExtra("food_id_list", food_id_list);
+        System.out.println("portion list in portion size activity = " + portion_list);
+
         intent.putExtra("portion_id_list", portion_id_list);
         intent.putExtra("portion_list", portion_list);
         intent.putExtra("picType", picType);
@@ -129,21 +125,10 @@ public class SelectPlatePortionSizeActivity extends AppCompatActivity {
         intent.putExtra("dishType", dishType);
         intent.putExtra("recipeCodeList", recipeCodeList);
         intent.putExtra("filename", filename);
-        intent.putExtra("recipeList", recipeList);
-
+        intent.putExtra("recipeNameList", recipeNameList);
 
         startActivity(intent);
     }
-/*
-    public void undoAddFood(View view) {
-        Intent intent = new Intent(this, SelectFoodActivity.class);
-        food_list.remove(food_name);
-        intent.putExtra("food_list", food_list);
-        intent.putExtra("food_id_list", food_id_list);
-        intent.putExtra("portion_list", portion_list);
-        startActivity(intent);
-    }
 
- */
 
 }
